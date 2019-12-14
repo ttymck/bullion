84d0126f6bb9026eaf197ddab02bb0663d70b9f6
ttymck <tyler@tylerkontra.com>
2019-12-14 12:59:44 -0800

    feat(game): new game, lookup game (w/ 404)

--- bullion/lib/bullion_web/controllers/game_controller.ex
+++ bullion/lib/bullion_web/controllers/game_controller.ex
@@ -1,11 +1,35 @@
 defmodule BullionWeb.GameController do
   use BullionWeb, :controller
 
+  alias Bullion.Repo
+  alias Bullion.Game
+
   def new(conn, _params) do
-    render(conn, "new_game.html")
+    new_game = %Game{
+      shortcode: "abc123", buyin_dollars: 20, buyin_chips: 100
+    } |> Repo.insert
+    conn
+    |> assign(:game, new_game)
+    |> render("new_game.html")
+  end
+
+ defp get_game_by_shortcode(shortcode) do
+    Game 
+    |> Repo.get_by(shortcode: shortcode)
+    |> case do
+        nil -> {:error, :game_not_found}
+        user -> {:ok, user}
+    end
   end
 
   def lookup(conn, %{"shortcode" => shortcode}) do
-    render(conn, "view_game.html", shortcode: shortcode)
+    case get_game_by_shortcode(shortcode) do
+      {:error, _} -> conn
+        |> put_flash(:info, "Game not found for shortcode '#{shortcode}'")
+        |> redirect(to: "/")
+      {:ok, game} -> conn
+        |> assign(:game, game)
+        |> render("view_game.html", game: game)
+    end
   end
 end
